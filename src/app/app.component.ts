180c645987fab1be5986cf2ea36072790d0d41c7
Amine <amine.toualbi@yahoo.fr>
2019-10-22 21:09:16 -0600

    Call to OpenWeatherMap API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, Inject} from '@angular/core';
+import {AfterViewInit, Component, Inject, APP_ID} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { callbackify } from 'util';
 import { LocationStrategy } from '@angular/common';
@@ -13,6 +13,7 @@ export class AppComponent implements AfterViewInit {
 
   public weatherCondition: string;
   public cityTag: HTMLElement;
+  public API_KEY: string = "8300f2d4182612b5d44c3fcb22ca0acc"
 
   constructor(private httpClient: HttpClient) {}
 
@@ -25,7 +26,10 @@ export class AppComponent implements AfterViewInit {
     if(navigator.geolocation) {
       let lat = navigator.geolocation.getCurrentPosition(position => {
         console.log(position);
-        this.updateCity(position.coords.latitude, position.coords.longitude);
+        let lat: number = position.coords.latitude;
+        let long: number = position.coords.longitude;
+        this.updateCity(lat, long);
+        this.getWeatherData(lat, long)
       })
     }
     else {
@@ -33,13 +37,32 @@ export class AppComponent implements AfterViewInit {
     }
   }
 
+  //api.openweathermap.org/data/2.5/weather?lat=35&lon=139
+  public getWeatherData(lat: number, long: number) {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const API_URL = proxy+"https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid="+this.API_KEY;
+    fetch(API_URL)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+        });
+  }
+
   public updateCity(lat: number, long: number) {
     const reverse = require('reverse-geocode')
     let geoDataString : string = JSON.stringify(reverse.lookup(lat, long, 'us')); 
     let geoData = JSON.parse(geoDataString);
     let city = geoData.city; 
+    this.updateUI(city)
+
+  }
+
+  public updateUI(city: string) {
     this.cityTag.innerHTML = city;
   }
+
 
 
 
